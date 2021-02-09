a51deaf0f48ab7288869d8a5268df4e5b316f55a
Igor Balanici <Igor.Balanici@endava.com>
2021-02-09 17:45:53 +0200

    Small changes for transitive

--- staff/src/module-info.java
+++ staff/src/module-info.java
@@ -1,5 +1,3 @@
 module staff {
-//    requires feeding;
-//    requires care;
     requires talks;
 }
--- talks/src/module-info.java
+++ talks/src/module-info.java
@@ -1,8 +1,7 @@
 module talks {
-    exports zoo.animal.talks.content;
+    exports zoo.animal.talks.content to staff;
     exports zoo.animal.talks.media;
     exports zoo.animal.talks.schedule;
 
-//    requires feeding;
     requires transitive care;
 }
